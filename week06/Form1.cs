20c5040810c8b750f4f153d31274736c90d598cc
Tamás Márton <mtomi0103@gmail.com>
2020-10-18 17:03:07 +0200

    feladat no4

--- week06/Form1.cs
+++ week06/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.MnbServiceReference;
 
 namespace week06
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates;
+
         public Form1()
         {
+
             InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+
+            var response = mnbService.GetExchangeRates(request);
+
+           
+            var result = response.GetExchangeRatesResult;
+
+
+            dataGridView1.DataSource = Rates;
+
         }
+
+        
+
     }
 }
