36c12a05c73110548424f97fde734adee7ab22dc
Tamás Márton <mtomi0103@gmail.com>
2020-10-25 15:06:13 +0100

    feladat no8

--- week07/Form1.cs
+++ week07/Form1.cs
@@ -22,27 +22,8 @@ namespace week07
         public Form1()
         {
             InitializeComponent();
-
-            Population = GetPopulation(@"C:\Windows\Temp\nép.csv");
-            BirthProbabilities = GetBirthProbabilities(@"C:\Windows\Temp\születés.csv");
-            DeathProbabilities = GetDeathProbabilities(@"C:\Windows\Temp\halál.csv");
-
-            for (int year = 2005; year <= 2024; year++)
-            {
-                for (int i = 0; i < Population.Count; i++)
-                {
-
-                }
-
-                int nbrOfMales = (from x in Population
-                                  where x.Gender == Gender.Male && x.IsAlive
-                                  select x).Count();
-                int nbrOfFemales = (from x in Population
-                                    where x.Gender == Gender.Female && x.IsAlive
-                                    select x).Count();
-
-                Console.WriteLine(string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
-            }
+            
+            
 
         }
 
@@ -107,5 +88,89 @@ namespace week07
             return deathProbabilities;
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Simualtion();
+        }
+
+        void Simualtion()
+        {
+            Population = GetPopulation(@"C:\Windows\Temp\nép.csv");
+            BirthProbabilities = GetBirthProbabilities(@"C:\Windows\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Windows\Temp\halál.csv");
+
+            List<Person> Males = new List<Person>();
+            List<Person> Females = new List<Person>();
+
+            for (int y = 2005; y <= 2024; y++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    void SimStep(int year, Person person)
+                    {
+                        if (!person.IsAlive) return;
+
+                        byte age = (byte)(year - person.BirthYear);
+
+                        double pDeath = (from x in DeathProbabilities
+                                         where x.Gender == person.Gender && x.BirthYear == age
+                                         select x.DeathP).FirstOrDefault();
+
+                        if (rng.NextDouble() <= pDeath)
+                        {
+                            person.IsAlive = false;
+                        }
+
+                        if (person.IsAlive && person.Gender == Gender.Female)
+                        {
+                            double pBirth = (from x in BirthProbabilities
+                                             where x.BirthYear == age
+                                             select x.BirthP).FirstOrDefault();
+                            if (rng.NextDouble() <= pBirth)
+                            {
+                                Person újszülött = new Person();
+                                újszülött.BirthYear = year;
+                                újszülött.NbrOfChildren = 0;
+                                újszülött.Gender = (Gender)(rng.Next(1, 3));
+                                Population.Add(újszülött);
+                            }
+                        }
+
+                        if (person.Gender == Gender.Male)
+                        {
+                            Males.Add(new Person { });
+                        }
+
+                        if (person.Gender == Gender.Female)
+                        {
+                            Females.Add(new Person { });
+                        }
+                    }
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                
+                Console.WriteLine(string.Format("Év:{0} Fiúk:{1} Lányok:{2}", y, nbrOfMales, nbrOfFemales));
+            }
+
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string result = openFileDialog1.FileName.ToString();
+            textBox1.Text = result.ToString();
+            
+            
+        }
     }
+
 }
