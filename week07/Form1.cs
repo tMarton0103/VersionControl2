08d6f5ec9a4f866c8a72bbafc1808b5b08af07d4
Tamás Márton <mtomi0103@gmail.com>
2020-10-25 13:10:59 +0100

    feladat05

--- week07/Form1.cs
+++ week07/Form1.cs
@@ -17,6 +17,8 @@ namespace week07
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
+        Random rng = new Random(1234);
+
         public Form1()
         {
             InitializeComponent();
