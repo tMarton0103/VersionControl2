bdeb2e2eb9972ca8d0e5fd9e5832feb2d05696f8
Tamás Márton <mtomi0103@gmail.com>
2020-10-25 12:46:04 +0100

    feladat03

---
+++ week07/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07
+{
+    public class BirthProbability
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double BirthP { get; set; }
+    }
+}
---
+++ week07/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int BirthYear { get; set; }
+        public double DeathP { get; set; }
+    }
+}
