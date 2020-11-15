caadef25f64e280f13192ecbecb83e18373beff2
Tamás Márton <mtomi0103@gmail.com>
2020-11-15 14:20:21 +0100

    feladat no13

--- week08/Ball.cs
+++ week08/Ball.cs
@@ -11,13 +11,18 @@ namespace week08
 {
     public class Ball : Toy
     {
-       
+        public SolidBrush BallColor { get; private set; }
+
+        public Ball(Color color)
+        {
+            BallColor = new SolidBrush(color);
+        }
 
         protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
 
-       
+          
     }
 }
--- week08/Form1.cs
+++ week08/Form1.cs
@@ -61,7 +61,10 @@ namespace week08
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory
+            {
+                BallColor = btnColor.BackColor
+            };
         }
 
         private void button1_Click(object sender, EventArgs e)
