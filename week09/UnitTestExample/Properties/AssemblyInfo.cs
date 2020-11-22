4945df70dc580a2bfb34c65b644f73f165a411ad
Tamás Márton <mtomi0103@gmail.com>
2020-11-22 12:34:28 +0100

    feladat no1

--- week09/UnitTestExample/Properties/AssemblyInfo.cs
+++ week09/UnitTestExample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("week09")]
+[assembly: AssemblyTitle("UnitTestExample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("week09")]
+[assembly: AssemblyProduct("UnitTestExample")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0ca15797-e5fe-4c88-a917-3730cf97170c")]
+[assembly: Guid("f1fbdbb6-feba-421b-a647-484ed380e500")]
 
 // Version information for an assembly consists of the following four values:
 //
