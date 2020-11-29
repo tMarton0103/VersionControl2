88af3a819776ecfb56669db11ad39042ff54bc22
Tamás Márton <mtomi0103@gmail.com>
2020-11-29 13:10:31 +0100

    feladat no4

--- week10/Form1.cs
+++ week10/Form1.cs
@@ -16,12 +16,25 @@ namespace week10
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+                gc.Start();
+            }
+         
+           
         }
     }
 }
